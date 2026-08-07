eca71face39a60ec00202c43f747c846e03d5fcf
wkarts <57051272+wkarts@users.noreply.github.com>
2026-08-07 02:58:01 -0300

    feat(web): carregar estilos do console operacional

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 import router from './router'
 import './styles.css'
 import './resource-management.css'
+import './operational-console.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
